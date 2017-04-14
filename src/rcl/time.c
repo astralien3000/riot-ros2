7babfa84d606c57d27697655cc2f55cc602313f4
Loïc Dauphin <astralien3000@yahoo.fr>
2017-04-14 14:06:17 +0200

    improved RCL time implementation

--- src/rcl/time.c
+++ src/rcl/time.c
@@ -12,35 +12,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "rcl/time.h"
+
 #include <stdbool.h>
 #include <stdlib.h>
 
-#include "rcl/time.h"
-
-#include "xtimer.h"
+#include "./stdatomic_helper.h"
 
 #include "./common.h"
 #include "rcl/error_handling.h"
-
-#define __WOULD_BE_NEGATIVE(seconds, subseconds) (seconds < 0 || (subseconds < 0 && seconds == 0))
-
-rcl_ret_t
-rcl_system_time_now(rcl_time_point_value_t * now)
-{
-  RCL_CHECK_ARGUMENT_FOR_NULL(now, RCL_RET_INVALID_ARGUMENT);
-  *now = xtimer_now_usec64();
-  return RCL_RET_OK;
-}
-
-rcl_ret_t
-rcl_steady_time_now(rcl_time_point_value_t * now)
-{
-  RCL_CHECK_ARGUMENT_FOR_NULL(now, RCL_RET_INVALID_ARGUMENT);
-  *now = xtimer_now_usec64();
-  return RCL_RET_OK;
-}
-
-#include "./stdatomic_helper.h"
 
 // Process default ROS time sources
 static rcl_time_source_t * rcl_default_ros_time_source;
