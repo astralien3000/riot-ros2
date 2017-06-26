19208acdea38d359eac22b0d595301f1044f6d87
Loïc Dauphin <astralien3000@yahoo.fr>
2017-06-26 17:39:11 +0200

    demos -> ros2

--- examples/gyro/main.cpp
+++ examples/gyro/main.cpp
@@ -7,15 +7,48 @@
 #include "task.h"
 #include "gyro.hpp"
 
+extern "C" {
+#include <rclc/rclc.h>
+}
+
+#include <std_msgs/msg/string.h>
+
 int main(void) {
+  static int argc = 0;
+  static char **argv = NULL;
+
   task_init();
 
   xtimer_sleep(1);
   Gyro::instance().start();
 
+  rclc_init(argc, argv);
+  rclc_node_t* node = rclc_create_node("talker");
+  rclc_publisher_t* pub = rclc_create_publisher(node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, String), "chatter", 1);
+
+  std_msgs__msg__String msg;
+  char buff[64] = {0};
+  msg.data.data = buff;
+  msg.data.capacity = sizeof(buff);
+  msg.data.size = 0;
+
+  while (rclc_ok()) {
+    msg.data.size = snprintf(msg.data.data, msg.data.capacity, "%i", (int)(100*Gyro::instance().angle().get()));
+    if(msg.data.size > msg.data.capacity) msg.data.size = 0;
+
+    if(msg.data.data[msg.data.size] == '\0') {
+      printf("Publishing: '%s'\n", msg.data.data);
+    }
+
+    rclc_publish(pub, (const void*)&msg);
+
+    rclc_sleep_ms(500);
+  }
+
+  rclc_destroy_publisher(pub);
+  rclc_destroy_node(node);
+
   while(1) {
-    printf("%i\n", (int)(1000*Gyro::instance().angle().get()));
-    xtimer_sleep(2);
   }
   
   return 0;
--- examples/motors/main.cpp
+++ examples/motors/main.cpp
@@ -5,16 +5,38 @@
 #include <xtimer.h>
 #include "motors.hpp"
 
+extern "C" {
+#include <rclc/rclc.h>
+}
+
+#include <std_msgs/msg/string.h>
+
+void chatter_callback(const void* v_msg)
+{
+  const std_msgs__msg__String* msg = (const std_msgs__msg__String*)v_msg;
+  printf("I heard: [%s]\n", msg->data.data);
+
+  int angle = atoi(msg->data.data);
+
+  Motors::instance().left().put(angle);
+  Motors::instance().right().put(-angle);
+}
+
 int main(void) {
+  static int argc = 0;
+  static char **argv = NULL;
 
   Motors::instance().start();
-  //Motors::instance().left().put(-255);
-  while(1) {
-    Motors::instance().left().put(255);
-    xtimer_sleep(2);
-    Motors::instance().left().put(-255);
-    xtimer_sleep(2);
-  }
+
+  rclc_init(argc, argv);
+  rclc_node_t* node = rclc_create_node("listener");
+  rclc_subscription_t* sub =
+      rclc_create_subscription(node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, String), "chatter", chatter_callback, 1, false);
+
+  rclc_spin_node(node);
+
+  rclc_destroy_subscription(sub);
+  rclc_destroy_node(node);
   
   return 0;
 }
